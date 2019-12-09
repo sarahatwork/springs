d2f5c6cd3b6d6e63748041e212ae78015b17b353
Sarah Mogin <mogin@work.co>
2019-12-09 17:24:19 -0500

    eslint

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import s from './slider.module.css'
+import s from './slider.module.css';
 
 function Slider({ images }) {
   return (
     <div className={s.wrapper}>
-      {images.map(({ src, alt}) => (
+      {images.map(({ src, alt }) => (
         <div className={s.imgWrapper}>
           <img src={src} key={src} alt={alt} />
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Slider
+export default Slider;
