ce4e7dcc358a477cec43d987a5eec2681deaa466
Sarah Mogin <mogin@work.co>
2019-12-09 18:05:46 -0500

    wip, clamp, etc.

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useSprings, animated } from 'react-spring';
+import { useDrag } from 'react-use-gesture';
+import clamp from 'just-clamp';
 
 import s from './slider.module.css';
 
 function Slider({ images }) {
+  const index = useRef(0);
+  const bind = useDrag(
+    ({ down, movement: [mx], direction: [xDir], distance, cancel }) => {
+      if (down && distance > 250) {
+        const newIndex = index.current + (xDir > 0 ? -1 : 1);
+        cancel((index.current = clamp(newIndex, 0, images.length - 1)));
+      }
+      set(i => {
+        const x = i - index.current * 500 + (down ? mx : 0);
+        return { scale: down ? 1 : 1, x };
+      });
+    }
+  );
+  const [springs, set] = useSprings(images.length, i => ({
+    scale: 1,
+    x: 0,
+    display: 'block'
+  }));
+
+  console.log(springs);
+
   return (
-    <div className={s.wrapper}>
-      {images.map(({ src, alt }) => (
-        <div className={s.imgWrapper}>
-          <img src={src} key={src} alt={alt} />
-        </div>
-      ))}
+    <div {...bind()} className={s.wrapper}>
+      {springs.map(({ display, scale, x }, i) => {
+        const { src, alt } = images[i];
+        return (
+          <animated.div
+            className={s.imgWrapper}
+            key={src}
+            style={{
+              display,
+              transform: x.interpolate(x => `translate3d(${x}px,0,0)`)
+            }}
+          >
+            <animated.img
+              style={{ transform: scale.interpolate(s => `scale(${s})`) }}
+              src={src}
+              alt={alt}
+            />
+          </animated.div>
+        );
+      })}
     </div>
   );
 }
