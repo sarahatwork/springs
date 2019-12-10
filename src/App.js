9cd95a49c0c2e6dc569f1261f794eb4ee53b5d6f
Sarah Mogin <mogin@work.co>
2019-12-10 10:44:05 -0500

    Implement Reach Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Router, Link } from '@reach/router';
 
 import Slider from './components/slider';
 
-import chi1 from './chi1.jpeg'
-import chi2 from './chi2.jpeg'
-import chi3 from './chi3.jpeg'
+import chi1 from './chi1.jpeg';
+import chi2 from './chi2.jpeg';
+import chi3 from './chi3.jpeg';
 
 const IMAGES = [
   {
@@ -19,11 +20,18 @@ const IMAGES = [
     src: chi3,
     alt: 'chi3'
   }
-]
+];
 
 function App() {
   return (
-     <Slider images={IMAGES} />
+    <>
+      <nav>
+        <Link to="/">home</Link> <Link to="/slider">slider</Link>
+      </nav>
+      <Router>
+        <Slider images={IMAGES} path="/slider" />
+      </Router>
+    </>
   );
 }
 
